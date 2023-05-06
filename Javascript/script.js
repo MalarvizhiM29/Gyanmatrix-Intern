b780682b5140fd054d721f5ce7ab4b3f46e52cb2
MALARVIZHI M <Malarm2004@gmail.com>
2023-05-06 23:44:23 +0530

    Basic JavaScript

---
+++ Javascript/script.js
@@ -0,0 +1,52 @@
+// Datatypes
+
+var Myname = "Malar"
+Myname =24
+
+let Intern = "Gyanmatrix"
+
+const pi=3.14
+
+
+//Storing values with assignment operator
+
+var a;
+var b = 2;
+console.log(a)
+a=7;
+b=a;
+console.log(a)
+
+
+//Initializing variables
+
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + "String!";
+
+
+//Arithmetic Operation
+
+var sum = 10 + 20;
+var diff = 30 - 20;
+var pro = 5 * 5;
+var quo = 66/0;
+
+console.log(sum)
+console.log(diff)
+console.log(pro)
+console.log(quo)
+
+//Incrementing & Decrementing
+
+var myvar = 5;
+myvar--;
+console.log(myvar)
+
+var myvar1 = 10;
+myvar1++;
+console.log(myvar1)
